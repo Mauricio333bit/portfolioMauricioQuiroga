577529e21fd6508a9bb7355379a24f2cd11eb6e7
Mauricio Quiroga <mauriquiroga97@gmail.com>
2024-01-08 00:19:38 -0300

    rebuild

--- .netlify/functions-internal/ssr/entry.mjs
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -1,9 +1,9 @@
 import * as adapter from '@astrojs/netlify/ssr-function.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_QXEUkZ1a.mjs';
+import { manifest } from './manifest_Vm4rXzXK.mjs';
 
 const _page0  = () => import('./chunks/generic_nBWIk4tP.mjs');
-const _page1  = () => import('./chunks/index_YIS8e2QG.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1]]);
+const _page1  = () => import('./chunks/index_sgqZzBy5.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
